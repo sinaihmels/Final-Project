406d46e217b4b75cf546be6717809fdac640da5a
sinaihmels <sinaihmels@gmail.com>
2024-02-19 14:04:50 +0100

    Create security.py for hashing passwords  login

--- app.py
+++ app.py
@@ -6,15 +6,42 @@ app = Flask(__name__) #we initiate the Flask application
 def index():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login" methods="GET", "POST")
 def login():
-    return render_template('login.html')
-    #write the login function
+    session.clear()
+
+    if request.method="GET":
+        render_template('login.html')
+
+    if request.method="POST":
+        # check that input is not empty 
+        if not request.form.get("username"):
+            return apology("Must input a username.", 403)
+
+        if not request.form.get("password"):
+            return apology("Must input a password.", 403)
+        
+        # get the data from the matching username from the database and store this in the dictionary data
+        data = db.execute(
+            "SELECT * FROM users WHERE username=?", request.form.get("username")
+        )
+        if len(data) != 1:
+            return apology("Invalid username.", 403)
+
+        # put the hash password from the database and the password that was in the input into the check_password_hash function
+        if not check_password_hash(data[0]["hash"], request.form.get("password")):
+            return apology("Invalid password.", 403)
+        else:
+            # if true log the user in and redirect to index
+            session["user_id"] = rows[0]["id"]
+            return redirect("/")
+        
 
 @app.route("/logout")
 def logout():
     return render_template('logout.html')
-    #write the logout function
+    # forget who was logged in
+    # redirect to log in
 
 @app.route("/settings")
 def settings():
@@ -24,4 +51,9 @@ def settings():
 @app.route("/register")
 def register():
     return render_template('register.html')
-    #write the register function
+    # check that the iput is not empty
+    # check that the password and the confirmation are identical
+    # hash the password with the hash_password function
+    # Input the username and the hashed password into the table users 
+    # log the user in
+    # redirect to index
---
+++ security.py
@@ -0,0 +1,24 @@
+import bcrypt 
+
+def hash_password(password: str):
+    password = password 
+    # converting password to array of bytes 
+    bytes = password.encode('utf-8') 
+    # generating the salt 
+    salt = bcrypt.gensalt() 
+    # Hashing the password 
+    hash = bcrypt.hashpw(bytes, salt) 
+    return hash
+
+
+# The function check_password_hash takes the hashed password from a specific username from the sql database and compares it with the hashed password given
+def check_password_hash(hashuser: str, passwordgiven: str) -> bool:
+    hash = hashuser
+    # Taking user entered password 
+    userPassword = passwordgiven
+    # encoding user password 
+    userBytes = userPassword.encode('utf-8') 
+    # checking password 
+    result = bcrypt.checkpw(userBytes, hash) 
+    print(result)
+
