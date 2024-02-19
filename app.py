a6bec59ba2a357b72dca45542423daa99f4806b4
sinaihmels <sinaihmels@gmail.com>
2024-02-19 09:18:01 +0100

    Changes from the 18.02.

--- app.py
+++ app.py
@@ -4,5 +4,24 @@ app = Flask(__name__) #we initiate the Flask application
 
 @app.route('/') #the / is the decorator (what to put into the URL) the / stands for the index page
 def index():
-
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+    #write the login function
+
+@app.route("/logout")
+def logout():
+    return render_template('logout.html')
+    #write the logout function
+
+@app.route("/settings")
+def settings():
+    return render_template('settings.html')
+    #write the settings function
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+    #write the register function
